813ab8177230e33b334077e4a0efbb641d32021d
andre.scarvalho1 <andre.scarvalho1@TAT0541242W10-1.senacsp.edu.br>
2019-06-03 19:36:15 -0300

    PDV

--- Fundamentos/src/PDV.java
+++ Fundamentos/src/PDV.java
@@ -1,15 +1,36 @@
 import java.util.Date;
+import java.util.Scanner;
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 
 public class PDV {
 
 	public static void main(String[] args) {
 		//Date ->trabalhar com Data
-		Date data = new Date();
+		Date data = new Date();		
 		//formatar a data
-		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL); 
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		DecimalFormat resultado = new DecimalFormat("0.00");
+		double total, desconto, totalDesconto, pago, troco;
+		Scanner leia = new Scanner(System.in);
 		System.out.println(formatador.format(data));
-
+		System.out.println("==============");
+		System.out.println("===== PDV ====");
+		System.out.println("==============");
+		System.out.println("");
+		System.out.println("Valor total");
+		total = leia.nextDouble();
+		System.out.println("Desconto em %");
+		desconto = leia.nextDouble();
+		totalDesconto = total - (desconto * total) / 100; 
+		System.out.println("Total com desconto: "+ resultado.format(totalDesconto)); 
+		System.out.println("__________________________________");
+		System.out.println("");
+		System.out.println("Valor pago");
+		pago = leia.nextDouble();
+		troco = pago - totalDesconto; 
+		System.out.println("Troco " + resultado.format(troco)); 
+		
 	}
 
 }
