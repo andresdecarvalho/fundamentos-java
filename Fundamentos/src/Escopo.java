a185f10f2e0b8621c58d429830e461c1e0035964
andre.scarvalho1 <andre.scarvalho1@TAT0541242W10-1.senacsp.edu.br>
2019-06-07 19:19:53 -0300

    Adicionado ao Escopo, Estudo While, Tabuada

---
+++ Fundamentos/src/Escopo.java
@@ -0,0 +1,20 @@
+
+public class Escopo {
+
+	public static void main(String[] args) {
+		// No Java toda variável precisa ser iniciada
+		// variável do tipo inteiro
+		int x;
+		boolean gameOver;
+		// variável com valor
+		x = 0;
+		System.out.println(x);
+		if (x == 0) {
+			gameOver = false;
+		} else {
+			gameOver = true;
+		}
+		System.out.println(gameOver);
+	}
+
+}
---
+++ Fundamentos/src/EstudoWhile.java
@@ -0,0 +1,17 @@
+
+public class EstudoWhile {
+
+	public static void main(String[] args) {
+		//Estudo da estruturas while / do while 
+		int x = 2;
+		while (x == 2) {
+			System.out.println("Teste while");
+		}
+		
+		do {
+			System.out.println("Teste do while");
+		} while (x == 1); 
+
+	}
+
+}
